c1d1be664b1dde60c0f493f9a1635442b9d28b55
IhebKraiem <142820696+linkinwaydevops@users.noreply.github.com>
2024-12-18 00:47:47 +0100

    dfsdf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,15 +17,8 @@ export class HomeComponent implements OnInit {
   navigateToPortfolio() {
     this.router.navigateByUrl('/portfolio-list').then(() => {
       
+      window.location.reload();
     });
-  }
-  navigateToServices() {
-    this.router.navigateByUrl('/services').then(() => {
-      
-    });
-  }
-  redirectToInstagram() {
-    window.open('https://www.instagram.com/bedouinsstudios/', '_blank', 'noopener,noreferrer');
   }
   navigateToHome() {
     this.router.navigateByUrl('/home').then(() => {
@@ -33,4 +26,14 @@ export class HomeComponent implements OnInit {
       window.location.reload();
     });
   }
+  navigateToServices() {
+    this.router.navigateByUrl('/services').then(() => {
+      
+      window.location.reload();
+    });
+  }
+  redirectToInstagram() {
+    window.open('https://www.instagram.com/bedouinsstudios/', '_blank', 'noopener,noreferrer');
+  }
+
 }
